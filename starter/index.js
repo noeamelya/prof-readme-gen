a1e254e519edd04e9914cae391d74d227c9d6852
noeamelya <noeamelyaofficial@hotmail.com>
2023-09-03 22:57:43 +0100

    add questions for the project

--- starter/index.js
+++ starter/index.js
@@ -4,17 +4,144 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
+const questions = [   
+// Project name
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of the project?',
+    validate: titleInput => {
+        if (titleInput) {
+            return true;
+        } else {
+            console.log('You need to enter a title to continue!');
+            return false;
+        }
+    }
+},
+// Description of project
+{
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of the project',
+    validate: descriptionInput => {
+        if (descriptionInput) {
+            return true;
+        } else {
+            console.log('You need to provide a project description!');
+            return false;
+        }
+    }
+},
+// Installation Instructions
+{
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install your project?',
+    validate: installationInput => {
+        if (installationInput) {
+            return true;
+        } else {
+            console.log('You need to provide installation info to continue!');
+            return false;
+        }
+    }
+},
+// Usage Information
+{
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this project?',
+    validate: usageInput => {
+        if (usageInput) {
+            return true;
+        } else {
+            console.log('You need to provide information on how to use project!');
+            return false;
+        }
+    }
+},
+// Contribution Guidlines
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'How should people contribute to this project?',
+    validate: contributionInput => {
+        if (contributionInput) {
+            return true;
+        } else {
+            console.log('You need to provide information on how to contribute to the project!');
+            return false;
+        }
+    }
+},
+// License Options
+{
+    type: 'checkbox',
+    name: 'licensing',
+    message: 'Choose a license for your project',
+    validate: licensingInput => {
+        if (licensingInput) {
+            return true;
+        } else {
+            console.log('You must pick a license for the project!');
+            return false;
+        }
+    }
+},
+// Test Instructions 
+{
+    type: 'input',
+    name: 'testing',
+    message: 'How do you test this project? (Required)',
+    validate: testingInput => {
+        if (testingInput) {
+            return true;
+        } else {
+            console.log('You need to describe how to test this project!');
+            return false;
+        }
+    }
+},
+// Github Username :
+{
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username',
+    validate: githubInput => {
+        if (githubInput) {
+            return true;
+        } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+        }
+    }
+},
+// Email Address :
+{
+    type: 'input',
+    name: 'email',
+    message: 'Would you like to include your email?',
+},
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('Success! Information transferred to the README!')
+    });
+};
+
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
 }
 
 // function call to initialize program
